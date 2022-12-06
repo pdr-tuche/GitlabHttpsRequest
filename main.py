8ceb48e0d6e8b47ab54d4569d056103c7d12e89e
Pedro Neves <55400486+pdr-tuche@users.noreply.github.com>
2022-12-06 11:40:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests
 
 user_name = input("digite aqui o seu usuário do gitlab: ")
-response = requests.get(f"https://gitlab.com/api/v4/users/{user_name}/projects") #type -> <class 'requests.models.Response'>
-lista_meus_projetos = response.json() #type -> <class 'list'> O metodo json converte o tipo para lista
+response = requests.get(f"https://gitlab.com/api/v4/users/{user_name}/projects")
+
+lista_meus_projetos = response.json()
 
 for projeto in lista_meus_projetos:
     print(f"nome do projeto: {projeto['name']} \nURL do projeto: {projeto['web_url']}\n")
